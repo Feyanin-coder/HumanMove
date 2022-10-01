5d79246a461a5a4665cdc868094f117db4307578
Feyanin-coder <ivmail27@gmail.com>
2022-10-01 11:59:17 +0200

    Committing code for simple move

--- Sources/HumanMove/HumanMove.swift
+++ Sources/HumanMove/HumanMove.swift
@@ -1,6 +1,16 @@
-public struct HumanMove {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+public class Human {
+    var name: String
+    var height: Int
+    var weight: Int
+    var gender: String
+    init(name: String, height: Int, weight: Int, gender: String) {
+        self.name = name
+        self.height = height
+        self.weight = weight
+        self.gender = gender
+    }
+    func move() {
+        print("Я хожу")
     }
 }
+public var moveHuman = Human(name: "Iggy", height: 196, weight: 70, gender: "male")
